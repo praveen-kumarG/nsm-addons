f2f84a49da52acb5255aeb36730a256fef86d8f8
Willem Hulshof <w.hulshof@magnus.nl>
2015-03-31 12:01:08 +0200

    import from diatomite

--- nsm_holidays/hr_holidays.py
+++ nsm_holidays/hr_holidays.py
@@ -21,7 +21,7 @@
 #
 ##############################################################################
 
-import datetime
+from datetime import *
 import time
 from itertools import groupby
 from operator import itemgetter
