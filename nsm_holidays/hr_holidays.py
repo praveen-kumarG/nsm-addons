a219700b3c1a0be4ee8d3c2b93661fdfddb9cf83
Robin Gabriner <r.gabriner@eurogroupconsulting.nl>
2015-04-02 13:04:01 +0200

    heel sql constraints gepakt

--- nsm_holidays/hr_holidays.py
+++ nsm_holidays/hr_holidays.py
@@ -35,7 +35,12 @@ from openerp.tools.translate import _
 
 class hr_holidays(osv.osv):
     _inherit = 'hr.holidays'
-    _sql_constraints[2]=('date_check', 'CHECK ( number_of_days_temp >= 0 )', 'The number of hours must be greater than 0.')
+    _sql_constraints = [
+        ('type_value', "CHECK( (holiday_type='employee' AND employee_id IS NOT NULL) or (holiday_type='category' AND category_id IS NOT NULL))", 
+         "The employee or employee category of this request is missing. Please make sure that your user login is linked to an employee."),
+        ('date_check2', "CHECK ( (type='add') OR (date_from <= date_to))", "The start date must be anterior to the end date."),
+        ('date_check', "CHECK ( number_of_days_temp >= 0 )", "The number of hours must be greater than 0."),
+    ]
     
     def _get_number_of_days(self, date_from, date_to):
         """Returns a float equals to the timedelta between two dates given as string."""
