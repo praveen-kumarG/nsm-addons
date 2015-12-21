8255562cdbef83ae89b898605eace4287c46e73e
Willem Hulshof <w.hulshof@magnus.nl>
2015-12-21 19:32:56 +0100

    code in analytic_account_tree, snelzoeken op code

--- nsm_analytic_2/account_analytic.py
+++ nsm_analytic_2/account_analytic.py
@@ -34,6 +34,25 @@ class account_analytic(osv.osv):
     _sql_constraints = [
         ('code_uniq', 'unique(code)', 'Code Analytic Account must be unique!'),
     ]
+
+    def name_search(self, cr, uid, name, args=None, operator='ilike', context=None, limit=100):
+        if not args:
+            args=[]
+        if context is None:
+            context={}
+        if name:
+            account_ids = self.search(cr, uid, [('code', '=like', name + "%")] + args, limit=limit, context=context)
+            if not account_ids:
+                dom = []
+                for name2 in name.split('/'):
+                    name = name2.strip()
+                    account_ids = self.search(cr, uid, dom + [('name', operator, name)] + args, limit=limit, context=context)
+                    if not account_ids: break
+                    dom = [('parent_id','in',account_ids)]
+        else:
+            account_ids = self.search(cr, uid, args, limit=limit, context=context)
+        return self.name_get(cr, uid, account_ids, context=context)
+
 account_analytic()
 
 
