1f4c11a31630414e3395793f133c00fb1a9edd72
Willem Hulshof <w.hulshof@magnus.nl>
2016-03-17 14:55:42 +0100

    schrijfrechten in factuurregel en group facturatie/batalingen in account_move_line_form: alles behalve kostenplaats

---
+++ nsm_improv02/__openerp__.py
@@ -0,0 +1,52 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+# Copyright (c) 2013 Magnus - Willem Hulshof - www.magnus.nl
+#
+# WARNING: This program as such is intended to be used by professional
+# programmers who take the whole responsability of assessing all potential
+# consequences resulting from its eventual inadequacies and bugs.
+# End users who are looking for a ready-to-use solution with commercial
+# garantees and support are strongly adviced to contract a Free Software
+# Service Company like Veritos.
+#
+# This program is Free Software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+#
+##############################################################################
+
+{
+    'name': 'nsm_improv02',
+    'version': '1.0',
+    'category': 'validaties/required',
+    'description': """
+This module changes some fields in invoices, account_move and partner views in Odoo.
+=====================================================================================
+
+
+    """,
+    'author': 'Magnus - Willem Hulshof',
+    'website': 'http://www.magnus.nl',
+    'depends': ['account', 'megis_auth',
+                ],
+    'data': [ "account_view.xml",
+#             "account_invoice_view.xml",
+#             "res_partner_view.xml",
+#             "account_data.xml",
+             "ir.model.access.csv",
+             ],
+    'demo': [],
+    'installable': True
+}
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
