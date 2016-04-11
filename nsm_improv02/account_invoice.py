054701f7b7f59e46062f99d46c2e2b25f1e7b471
Willem Hulshof <w.hulshof@magnus.nl>
2016-04-12 01:04:12 +0200

    Nog een basis term

--- nsm_improv02/account_invoice.py
+++ nsm_improv02/account_invoice.py
@@ -94,14 +94,14 @@ class account_invoice(osv.osv):
             ('proforma','Pro-forma'),   
             ('proforma2','Pro-forma'),
             ('open','Open'),
-            ('auth','Goedgekeurd'),
+            ('auth','Authorized'),
             ('verified','Verified'),
             ('paid','Paid'),
             ('cancel','Cancelled'),
             ],'Status', select=True, readonly=True, track_visibility='onchange',
             help=' * The \'Draft\' status is used when a user is encoding a new and unconfirmed Invoice. \
             \n* The \'Pro-forma\' when invoice is in Pro-forma status,invoice does not have an invoice number. \
-            \n* The \'Goedgekeurd\' status is used when invoice is already posted, but not yet confirmed for payment. \
+            \n* The \'Authorized\' status is used when invoice is already posted, but not yet confirmed for payment. \
             \n* The \'Verified\' status is used when invoice is already authorized, but not yet confirmed for payment, because it is of higher value than Company Verification treshold. \
             \n* The \'Open\' status is used when user create invoice,a invoice number is generated.Its in open status till user does not pay invoice. \
             \n* The \'Paid\' status is set automatically when the invoice is paid. Its related journal entries may or may not be reconciled. \
