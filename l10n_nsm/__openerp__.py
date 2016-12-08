4bb17093676c0bacb8333ed1ee202f1095b960d8
Willem Hulshof <w.hulshof@magnus.nl>
2016-12-08 01:32:24 +0100

    volgorde datafiles

--- l10n_nsm/__openerp__.py
+++ l10n_nsm/__openerp__.py
@@ -121,11 +121,10 @@ De extra cijfers worden dan achter het rekeningnummer aangevult met 'nullen'.
     'website' : 'http://www.magnus.nl',
     'depends' : ['l10n_nl',
     ],
-    'data' : ["account_chart_netherlands.xml",
-              'account.account.template.csv',
+    'data' : ['account.account.template.csv',
+              "account_chart_netherlands.xml",
               "account_fiscal_position_template.xml",
               "account_fiscal_position_tax_template.xml",
-
     ],
     'demo' : [],
     'installable': True,
