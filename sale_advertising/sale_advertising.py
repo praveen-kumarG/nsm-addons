253f9c09593587b5de18238e834d49a2f4689735
Willem Hulshof <w.hulshof@magnus.nl>
2017-03-07 15:20:53 +0100

    user_id (salesperson) not from partner, but just uid

--- sale_advertising/sale_advertising.py
+++ sale_advertising/sale_advertising.py
@@ -194,6 +194,7 @@ class sale_order(orm.Model):
                                              'Before saving the order the order lines and the total amounts may therefor'
                                              'show wrong values.')}
             res.update(result)
+        res['value']['user_id'] = uid
         return res
 
     def update_line_discount(self, cr, uid, ids, context=None):
@@ -389,6 +390,100 @@ class sale_advertising_proof(orm.Model):
     _defaults = {
         'number': 1,
     }
+
+class crm_make_sale(osv.osv_memory):
+    """ Make sale  order for crm """
+    _inherit = "crm.make.sale"
+
+    def makeOrder(self, cr, uid, ids, context=None):
+        """
+        This function  create Quotation on given case.
+        @param self: The object pointer
+        @param cr: the current row, from the database cursor,
+        @param uid: the current user’s ID for security checks,
+        @param ids: List of crm make sales' ids
+        @param context: A standard dictionary for contextual values
+        @return: Dictionary value of created sales order.
+
+        Override from sale_crm.crm_make_sale, in which user_id is not
+        taken from partner, but just uid.
+        """
+        if context is None:
+            context = {}
+        # update context: if come from phonecall, default state values can make the quote crash lp:1017353
+        context.pop('default_state', False)
+
+        case_obj = self.pool.get('crm.lead')
+        sale_obj = self.pool.get('sale.order')
+        partner_obj = self.pool.get('res.partner')
+        data = context and context.get('active_ids', []) or []
+
+        for make in self.browse(cr, uid, ids, context=context):
+            partner = make.partner_id
+            partner_addr = partner_obj.address_get(cr, uid, [partner.id],
+                                                   ['default', 'invoice', 'delivery', 'contact'])
+            pricelist = partner.property_product_pricelist.id
+            fpos = partner.property_account_position and partner.property_account_position.id or False
+            payment_term = partner.property_payment_term and partner.property_payment_term.id or False
+            new_ids = []
+            for case in case_obj.browse(cr, uid, data, context=context):
+                if not partner and case.partner_id:
+                    partner = case.partner_id
+                    fpos = partner.property_account_position and partner.property_account_position.id or False
+                    payment_term = partner.property_payment_term and partner.property_payment_term.id or False
+                    partner_addr = partner_obj.address_get(cr, uid, [partner.id],
+                                                           ['default', 'invoice', 'delivery', 'contact'])
+                    pricelist = partner.property_product_pricelist.id
+                if False in partner_addr.values():
+                    raise osv.except_osv(_('Insufficient Data!'), _('No address(es) defined for this customer.'))
+
+                vals = {
+                    'origin': _('Opportunity: %s') % str(case.id),
+                    'section_id': case.section_id and case.section_id.id or False,
+                    'categ_ids': [(6, 0, [categ_id.id for categ_id in case.categ_ids])],
+                    'shop_id': make.shop_id.id,
+                    'partner_id': partner.id,
+                    'pricelist_id': pricelist,
+                    'partner_invoice_id': partner_addr['invoice'],
+                    'partner_shipping_id': partner_addr['delivery'],
+                    'date_order': fields.date.context_today(self, cr, uid, context=context),
+                    'fiscal_position': fpos,
+                    'payment_term': payment_term,
+                    'user_id': uid,
+                }
+                new_id = sale_obj.create(cr, uid, vals, context=context)
+                sale_order = sale_obj.browse(cr, uid, new_id, context=context)
+                case_obj.write(cr, uid, [case.id], {'ref': 'sale.order,%s' % new_id})
+                new_ids.append(new_id)
+                message = _("Opportunity has been <b>converted</b> to the quotation <em>%s</em>.") % (sale_order.name)
+                case.message_post(body=message)
+            if make.close:
+                case_obj.case_close(cr, uid, data)
+            if not new_ids:
+                return {'type': 'ir.actions.act_window_close'}
+            if len(new_ids) <= 1:
+                value = {
+                    'domain': str([('id', 'in', new_ids)]),
+                    'view_type': 'form',
+                    'view_mode': 'form',
+                    'res_model': 'sale.order',
+                    'view_id': False,
+                    'type': 'ir.actions.act_window',
+                    'name': _('Quotation'),
+                    'res_id': new_ids and new_ids[0]
+                }
+            else:
+                value = {
+                    'domain': str([('id', 'in', new_ids)]),
+                    'view_type': 'form',
+                    'view_mode': 'tree,form',
+                    'res_model': 'sale.order',
+                    'view_id': False,
+                    'type': 'ir.actions.act_window',
+                    'name': _('Quotation'),
+                    'res_id': new_ids
+                }
+            return value
 
 
 
