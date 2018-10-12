b45a8c9020c721cd3e5c3b402c93a0ac92b03f7b
kumarinie <kumarisushmanie@gmail.com>
2018-10-12 15:27:12 +0530

    T0003 - Change field characteristics of proof_number_adv_customer in sale.advertising.order.line Branch: T0001-5 Repo: kumarinie/nsm-addons

--- nsm_sale_advertising_order/models/sale_advertising.py
+++ nsm_sale_advertising_order/models/sale_advertising.py
@@ -31,3 +31,4 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     proof_number_payer = fields.Many2one('res.partner', 'Proof Number Payer', domain=[('customer','=',True)])
+    proof_number_adv_customer = fields.Many2one('res.partner', 'Proof Number Advertising Customer', domain=[('customer','=',True)])
