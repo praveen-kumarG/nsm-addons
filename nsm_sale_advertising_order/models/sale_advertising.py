a3c0b19fb3791c281ff0f2ac8442c3c1e034168c
kumarinie <kumarisushmanie@gmail.com>
2018-10-12 15:43:30 +0530

    Task: T0005 - Create new field in sale.advertising.order.line: Proof Number Amount Advertiser Branch: T0001-5 Repo: kumarinie/nsm-addons

--- nsm_sale_advertising_order/models/sale_advertising.py
+++ nsm_sale_advertising_order/models/sale_advertising.py
@@ -33,3 +33,5 @@ class SaleOrderLine(models.Model):
     proof_number_payer = fields.Many2one('res.partner', 'Proof Number Payer', domain=[('customer','=',True)])
     proof_number_adv_customer = fields.Many2one('res.partner', 'Proof Number Advertising Customer', domain=[('customer','=',True)])
     proof_number_amt_payer = fields.Integer('Proof Number Amount Payer', default=1)
+    proof_number_amt_adv_customer = fields.Integer('Proof Number Amount Advertising', default=1)
+
