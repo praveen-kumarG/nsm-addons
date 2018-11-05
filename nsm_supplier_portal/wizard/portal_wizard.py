cf9cf60a49b7d055431799008f39b30b5d5b29d7
Stephan van de Berg <berg.stephan@gmail.com>
2018-11-05 13:49:58 +0100

    Did not see other imports

--- nsm_supplier_portal/wizard/portal_wizard.py
+++ nsm_supplier_portal/wizard/portal_wizard.py
@@ -19,6 +19,8 @@
 ##############################################################################
 
 
+from odoo import api, fields, models, _, SUPERUSER_ID
+from odoo.exceptions import UserError, ValidationError
 import logging
 logger = logging.getLogger(__name_)
 
