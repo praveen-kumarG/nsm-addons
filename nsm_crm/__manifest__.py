fa027470be6b98fbac43b7df67469072bff1e8ee
Lucie Bolt <l.bolt@magnus.nl>
2019-05-31 15:30:55 +0200

    nsm_crm: T0247 - Changes in Customers TreeView for NSM. Nog een keertje

--- nsm_crm/__manifest__.py
+++ nsm_crm/__manifest__.py
@@ -19,7 +19,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base', 'partner_sector', 'partner_firstname'],
+    'depends': ['sale_advertising_order', 'partner_sector', 'partner_firstname'],
 
     # always loaded
     'data': [
